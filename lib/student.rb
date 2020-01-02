09c5b93d1917bd35454db2988f0c6887fb38dddf
David Schoenholtz <dmschoenholtz@gmail.com>
2020-01-02 20:45:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,4 +116,18 @@ class Student
       end.first
     end
 
+    def self.all_students_in_grade_X
+      sql = <<-SQL
+        SELECT *
+        FROM students
+        WHERE grade = 10
+        ORDER BY students.id
+        LIMIT 1
+      SQL
+
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(row)
+      end.first
+    end
+
 end

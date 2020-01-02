7f3180de004e6be85987a51eff60285f380db8db
David Schoenholtz <dmschoenholtz@gmail.com>
2020-01-02 20:43:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -106,13 +106,11 @@ class Student
         SELECT *
         FROM students
         WHERE grade = 10
-        ORDER BY students.id
-        LIMIT ?
       SQL
 
       DB[:conn].execute(sql, number).map do |row|
         self.new_from_db(row)
-      end
+      end.first
     end
 
   end
